7cb76e8ef47d7bedfb91e4ca4c228950e53aae52
apprentice121jl <ju_longg@163.com>
2019-05-14 23:36:29 +0800

    park  unpark

---
+++ src/JDKCurrentUtil/ParkUnpark.java
@@ -0,0 +1,21 @@
+package JDKCurrentUtil;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * JL
+ * 2019/5/14  23:32
+ **/
+public class ParkUnpark {
+
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        LockSupport.unpark(t);
+        LockSupport.unpark(t);
+        LockSupport.park();
+        System.out.println("1");
+        LockSupport.park();
+        System.out.println("2");
+
+    }
+}
