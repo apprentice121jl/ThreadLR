026f52c8d02d2f620ae7a60965f2c9155185a8a4
居龙 <ju.long@sf-express.com>
2019-05-23 18:58:20 +0800

    park 对于 interrupt的响应

--- src/JDKCurrentUtil/LockSupport/TestPark.java
+++ src/JDKCurrentUtil/LockSupport/TestPark.java
@@ -2,6 +2,7 @@ package JDKCurrentUtil.LockSupport;
 
 import java.util.concurrent.locks.LockSupport;
 
+
 /**
  * park():阻塞线程
  * unpark():唤醒线程
@@ -18,10 +19,14 @@ public class TestPark {
 			public void run() {
 				System.out.println("线程开始执行");
 				LockSupport.park();
-				if(Thread.currentThread().isInterrupted()) {// 只是判断中断状态，并未清除状态，所以LockSupport.park();依旧不起作用
+				// Thread.currentThread().isInterrupted()  只是判断中断状态，并未清除状态，所以LockSupport.park();依旧不起作用
+				// Thread.interrupted() 获取中断状态的同时，将中断状态设置为false，所以LockSupport.park();起作用
+				if(Thread.interrupted()) {
+					System.out.println("进入中断判断");
 					LockSupport.park();
 					System.out.println("thread "+Thread.currentThread().getId()+" awake!");
 				}
+				System.out.println("线程结束执行");
 			}
 		};
 		
@@ -29,7 +34,9 @@ public class TestPark {
 		Thread.sleep(3000);
 		System.out.println("rrrrr");
 		t.interrupt();
-		// LockSupport.unpark(t);
+		//Thread.sleep(10000);
+		LockSupport.unpark(t);
+		System.out.println("执行unpark");
 	}
 	
 	
