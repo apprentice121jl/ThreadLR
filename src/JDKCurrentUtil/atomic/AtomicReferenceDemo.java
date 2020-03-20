017bec3a850a6e7b7634d3bc5cf41f430acbc659
apprentice121jl <ju_longg@163.com>
2020-03-20 09:58:31 +0800

    atomic

--- src/JDKCurrentUtil/atomic/AtomicReferenceDemo.java
+++ src/JDKCurrentUtil/atomic/AtomicReferenceDemo.java
@@ -20,6 +20,7 @@ public class AtomicReferenceDemo {
                         if (m < 20) {
                             if (money.compareAndSet(m, m + 20)) {
                                 System.out.println("余额小于20元，充值成功，余额：" + money.get() + " 元");
+                                break;
                             }
                         } else {
                             break;
@@ -31,28 +32,28 @@ public class AtomicReferenceDemo {
 
         }
 
-            new Thread(()->{
-                for (int j = 0; j < 100 ; j++) {
-                    while(true){
-                        Integer m = money.get();
-                        if(m > 10){
-                            System.out.println("大于10元");
-                            if(money.compareAndSet(m,m-10)){
-                                System.out.println("成功消费10元，余额："+money.get());
-                                break;
-                            }
-                        }else{
-                                System.out.println("没有足够金额");
-                                break;
+        new Thread(()->{
+            for (int j = 0; j < 100 ; j++) {
+                while(true){
+                    Integer m = money.get();
+                    if(m > 10){
+                        System.out.println("大于10元");
+                        if(money.compareAndSet(m,m-10)){
+                            System.out.println("成功消费10元，余额："+money.get());
+                            break;
                         }
+                    }else{
+                            System.out.println("没有足够金额");
+                            break;
                     }
                 }
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
 
     }
