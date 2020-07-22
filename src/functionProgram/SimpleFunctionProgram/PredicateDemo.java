6d2b04bc680d2517ce0f28da7e97c9313a6ed636
julong <julong@jdcom>
2020-07-22 16:06:03 +0800

    函数式编程

--- src/functionProgram/SimpleFunctionProgram/PredicateDemo.java
+++ src/functionProgram/SimpleFunctionProgram/PredicateDemo.java
@@ -7,21 +7,34 @@ import java.util.function.Predicate;
 
 /**
  * @author julong
- * @description 一个涉及类型T的布尔表达式时，就可以使用Predicate接口
- * @createTime 2020/7/15  15:35
+ * @description 一个涉及类型T的布尔表达式时，可以使用Predicate接口
+ *
+ * @see Predicate
+ *  default Predicate<T> or(Predicate<? super T> other) {
+ *         Objects.requireNonNull(other);
+ *         // 整个语句为test实现
+ *         return (t) -> test(t) || other.test(t);
+ *     }
  **/
 public class PredicateDemo {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("1", "2", "3", "11", "22", "33");
         Predicate<String> predicate = (String str) -> str.length() == 1;
+        // 过滤长度为1的字符串
         filter(list, predicate);
+        // 过滤长度非1的字符串
         negateFilter(list, predicate);
+        // 获取相同的数据
         equalFilter(list);
-        Predicate<String> orPredicate = (String str) -> "11".equals(str);
+
+        Predicate<String> orPredicate = str -> "11".equals(str);
         orFilter(list, predicate, orPredicate);
+
         Predicate<String> andPredicate = (String str) -> "1".equals(str);
         andFilter(list, predicate, andPredicate);
+        // 相当于 （predicate && andPredicate）|| orPredicate
         andOrFilter(list, predicate, andPredicate, orPredicate);
+        // 相当于  （predicate || orPredicate）&& andPredicate
         orAndFilter(list, predicate, andPredicate, orPredicate);
     }
 
@@ -58,7 +71,7 @@ public class PredicateDemo {
                 result.add(t);
             }
         }
-        System.out.println(result);
+        System.out.println("orFilter:"+result);
     }
 
     public static <T> void andFilter(List<T> list, Predicate<T> predicate, Predicate<T> andPredicate) {
